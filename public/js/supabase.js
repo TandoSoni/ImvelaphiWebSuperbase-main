b5c901a65f619d485b10d0d4db45c69f44b8ea1e
TANDO SONI <sonitando@gmail.com>
2026-09-09 15:57:05 +0200

    Configure Supabase public client

--- public/js/supabase.js
+++ public/js/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
 
 export const supabase = createClient(
-  'https://kfyixpqiqlwocemedd.supabase.co',
+  'https://kfyixpqiicqlwocemedd.supabase.co',
   'sb_publishable_fSddlEUWbN7xiFFYKskBfA_RPjP_IqJ'
 );
